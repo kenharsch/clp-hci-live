ad105504a7fb0bd1b22a303a69f76d3cfcbc6aa3
kenharsch <kharsch@brandeis.edu>
2015-10-19 11:43:03 -0400

    re-ordered assignments by due date

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -4,7 +4,7 @@ class AssignmentsController < ApplicationController
   # GET /assignments
   # GET /assignments.json
   def index
-    @assignments = Assignment.all
+    @assignments = Assignment.all.order(due: :asc)
   end
 
   # GET /assignments/1
